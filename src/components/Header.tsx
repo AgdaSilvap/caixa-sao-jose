50469e8b1f64cc5d5d3e754c2ca62a333a4c073b
Agda Silva <agdasilvap@gmail.com>
2025-04-26 12:23:43 -0300

    Melhora a estrutura do cabeçalho, ajustando o layout e a formatação do título

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between gap-2 p-4 bg-orange-teal-5 rounded-lg sticky top-0 z-10">
+    <header className="flex items-center gap-x-8 p-4 bg-orange-teal-5 rounded-lg sticky top-0 z-10">
       <img src="/img/sao-jose-operario.png" alt="São José Operário" className="w-16 h-16 object-contain rounded-lg bg-white/50 border-2 border-orange-teal-2" />
-      <h1 className="text-xl md:text-3xl font-bold tracking-tighter font-cursive">Comunidade São José Operário</h1>
+      <h1 className="text-2xl md:text-3xl tracking-tighter font-cursive">
+        <small className="block text-xl md:text-2xl">Comunidade</small>
+        <span className="font-bold">São José Operário</span>
+      </h1>
     </header>
   )
 }
