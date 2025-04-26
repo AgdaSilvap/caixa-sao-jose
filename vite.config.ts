20903ab551a439ef29ffe98050e45d7b594da3c7
Agda Silva <agdasilvap@gmail.com>
2025-04-26 12:04:54 -0300

    Adiciona plugin de PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Caixa São José Operário',
+        short_name: 'Caixa SJO',
+        description: 'Caixa São José Operário',
+        theme_color: '#348e8f',
+        background_color: '#ffffff',
+        display: 'standalone',
+        lang: 'pt-BR',
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-1024x1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    })
   ],
 })
