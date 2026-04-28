4cda1b73496df78ef95244e1d91b67f1fc8a7396
Douglas Gusson <douglasgusson@gmail.com>
2026-04-28 15:45:59 -0300

    Ajusta layout do botão de conclusão

--- src/components/BotaoConcluir.tsx
+++ src/components/BotaoConcluir.tsx
@@ -6,7 +6,7 @@ type BotaoConcluirProps = {
 export const BotaoConcluir = ({ onClick, disabled }: BotaoConcluirProps) => {
   return (
     <button
-      className="max-w-2xs bg-orange-teal-4 text-white px-4 py-2 rounded-lg disabled:opacity-50"
+      className="flex-1 bg-orange-teal-4 text-white px-4 py-2 rounded-lg disabled:opacity-50"
       disabled={disabled}
       onClick={onClick}
     >
